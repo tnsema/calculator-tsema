5fe41fdcf5cce4ef916ef743b270bebb7ebbd105
SemaTN <129980966+tnsema@users.noreply.github.com>
2023-12-16 10:12:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+alert("hey");
+const display = document.getElementById("display");
+
+prompt("rt");
+
+document.getElementById("num1").onclick = function(){
+
+    document.getElementById("display") = 1;
+
+}
+
+
+
+
+
+function appendToDisplay(input) {
+    
+    display = input;
+    
+
+}
+
+function clear() {
+    display = "";
+}
+
+
+
+
